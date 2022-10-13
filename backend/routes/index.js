e298e3e81dfadc94fd0eafda6e33774c9519f67b
Rudi Farkas <rudi.farkas@gmail.com>
2022-10-13 20:14:10 +0200

    touch up index.js: title and comments

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express hic et nunc' });
+  // 'index' is the name of the view file, views/index.jade
+  // per app.set("view engine", "jade");
 });
 
 module.exports = router;
